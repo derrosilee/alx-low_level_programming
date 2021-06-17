e3937e6a54855487aeab88caf5aed5c182c5f36b
derrosilee <derrosilee@gmail.com>
2021-06-17 22:54:25 +0300

    Low Level Programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always at 0
+ *
+ */
+int main(void) 
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
