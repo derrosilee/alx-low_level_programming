b5c8afad06a1ab9b0f629d13a3f6b94094964c18
derrosilee <derrosilee@gmail.com>
2021-06-18 22:55:18 +0300

    variables if else

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: (Success)
+ */
+int main(void)
+{
+
+  char c;
+
+  for (c = 'a'; c <= 'z'; c++)
+    putchar(c);
+  putchar('\n');
+
+  return (0);
+}
